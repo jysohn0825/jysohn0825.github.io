28bdb0447087f5197aa84cd3d955a712151c848f
jysohn0825 <jysohn0825@naver.com>
2024-02-15 23:26:28 +0900

    module export 수정

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,8 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  transpileDependencies: true,
-  outputDir: './docs',
-  publicPath: '/jysohn0825.github.io'
+  transpileDependencies: true
 })
+module.exports = {
+  outputDir: './docs',
+  publicPath: '/jysohn0825.github.io/'
+}
